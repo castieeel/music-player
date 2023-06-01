0478dbb64b1d9048c4d348163235b40d6a893558
Victoria Kiseleva <castieeel@hotmail.com>
2023-06-01 22:04:59 +0800

    BR5.3(drop-down)

--- src/components/main/centerblock/content/playlist/PlaylistItem.jsx
+++ src/components/main/centerblock/content/playlist/PlaylistItem.jsx
@@ -1,62 +1,48 @@
 import Icons from "../../../../../assets/img/icon/sprite.svg";
-import Skeleton from "react-loading-skeleton";
 import React from "react";
 import "react-loading-skeleton/dist/skeleton.css";
-import { useEffect, useState } from "react";
+import { SkeletonLoading } from "./SkeletonLoading";
 
 export const PlaylistItem = (props) => {
-  const [loading, setLoading] = useState(true);
-
-  //выполняется при первом рендере
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <div className="playlist__item">
-      {loading ? (
-        <div className="playlist__track track">
-          <Skeleton className="track__title" />
-          <Skeleton className="track__author" />
-          <Skeleton className="track__album" />
-          <Skeleton className="track__time" />
-        </div>
-      ) : (
-        <div className="playlist__track track">
-          <div className="track__title">
-            <div className="track__title-image">
-              <svg className="track__title-svg" alt="music">
-                <use xlinkHref={`${Icons}#icon-note`}></use>
-              </svg>
-            </div>
-            <div className="track__title-text">
+      <div className="playlist__track track">
+        <div className="track__title">
+          <div className="track__title-image">
+            <svg className="track__title-svg" alt="music">
+              <use xlinkHref={`${Icons}#icon-note`}></use>
+            </svg>
+          </div>
+          <div className="track__title-text">
+            <SkeletonLoading width={350} height={24}>
               <a className="track__title-link" href="http://">
                 {props.title}
                 <span className="track__title-span"></span>
               </a>
-            </div>
+            </SkeletonLoading>
           </div>
-          <div className="track__author">
+        </div>
+        <div className="track__author">
+          <SkeletonLoading width={300} height={25}>
             <a className="track__author-link" href="http://">
               {props.author}
             </a>
-          </div>
-          <div className="track__album">
+          </SkeletonLoading>
+        </div>
+        <div className="track__album">
+          <SkeletonLoading width={245} height={25}>
             <a className="track__album-link" href="http://">
               {props.album}
             </a>
-          </div>
-          <div className="track__time">
-            <svg className="track__time-svg" alt="time">
-              <use xlinkHref={`${Icons}#icon-like`}></use>
-            </svg>
-            <span className="track__time-text">{props.time} </span>
-          </div>
+          </SkeletonLoading>
         </div>
-      )}
+        <div className="track__time">
+          <svg className="track__time-svg" alt="time">
+            <use xlinkHref={`${Icons}#icon-like`}></use>
+          </svg>
+          <span className="track__time-text">{props.time} </span>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/main/centerblock/content/playlist/SkeletonLoading.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+export const SkeletonLoading = (props) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Установка задержки на 5 секунд для эмуляции загрузки
+    setTimeout(() => setLoading(false), 3000);
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        // Отображение компонента Skeleton для эмуляции загрузки
+        <Skeleton width={props.width} height={props.height} />
+      ) : (
+        // Отображение контента, переданного через props.children
+        <div>{props.children}</div>
+      )}
+    </div>
+  );
+};
