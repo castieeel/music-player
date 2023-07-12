b40cf38c705ef8cfbcce53fc26c68acc373d1723
Victoria Kiseleva <castieeel@hotmail.com>
2023-07-12 19:04:33 +0800

    BR5.8 todo

--- src/pages/playlist/index.jsx
+++ src/pages/playlist/index.jsx
@@ -1,7 +1,13 @@
+import { AddTodo } from "./add-todo";
+import { TodoList } from "./todo-list";
+
 export const Playlist = () => {
   return (
     <div>
       <h1>Playlist</h1>
+
+      <AddTodo />
+      <TodoList />
     </div>
   );
 };
